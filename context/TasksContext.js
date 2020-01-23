86013d335718325e10a363cbc541ac3238f19c8a
Rob Terrell <Rterrell25@gmail.com>
2020-01-23 15:49:47 -0500

    created the add task function within TasksContext

--- context/TasksContext.js
+++ context/TasksContext.js
@@ -3,13 +3,20 @@ import React, { createContext, useState } from "react";
 export const TasksContext = createContext();
 
 export const TasksContextProvider = ({ children }) => {
-  const dummyTasks = [
-    { description: "Learn React Native", id: "1" },
-    { description: "Learn React Context", id: "2" }
-  ];
-  const [tasks, setTasks] = useState(dummyTasks);
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = description => {
+    const newTask = {
+      description,
+      completed: false,
+      id: String(Number(new Date()))
+    };
+    setTasks([newTask, ...tasks]);
+  };
 
   return (
-    <TasksContext.Provider value={{ tasks }}>{children}</TasksContext.Provider>
+    <TasksContext.Provider value={{ tasks, addTask }}>
+      {children}
+    </TasksContext.Provider>
   );
 };
