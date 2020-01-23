d5ec1e922404e713d8e6717f6ef4e52ef415cdc9
Rob Terrell <Rterrell25@gmail.com>
2020-01-23 16:03:36 -0500

    added styling and a title to the form component to make the application aesthetically pleasing

--- components/Form.js
+++ components/Form.js
@@ -1,18 +1,33 @@
 import React, { useState } from "react";
-import { TextInput, StyleSheet } from "react-native";
+import { TextInput, View, Text, StyleSheet } from "react-native";
 
 export default () => {
   const [description, setDescription] = useState("");
   return (
-    <TextInput
-      style={styles.textInput}
-      onChangeText={text => setDescription(text)}
-      value={description}
-    />
+    <View style={styles.header}>
+      <Text style={styles.headerText}>My Tasks</Text>
+      <TextInput
+        style={styles.textInput}
+        onChangeText={text => setDescription(text)}
+        value={description}
+      />
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
+  header: {
+    backgroundColor: "whitesmoke",
+    padding: 10,
+    borderBottomColor: "gray",
+    borderBottomWidth: 1,
+    borderTopColor: "gray",
+    borderTopWidth: 1
+  },
+  headerText: {
+    fontSize: 30,
+    marginTop: 20
+  },
   textInput: {
     width: "100%",
     borderColor: "gray",
